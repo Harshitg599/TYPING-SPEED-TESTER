722b92272347ea51b50318b93cf1b2e563020667
Harshitg599 <83924148+Harshitg599@users.noreply.github.com>
2023-01-29 18:01:37 +0530

    Update Logo.js

--- src/components/common/Logo.js
+++ src/components/common/Logo.js
@@ -5,7 +5,7 @@ const Logo = ({ isFocusedMode, isMusicMode }) => {
   return (
     <div className="header" style={{visibility: isFocusedMode ? 'hidden' : 'visible' }}>
       <h1>
-        Touch Typing <KeyboardAltIcon fontSize="large" />
+        Typing Speed Tester <KeyboardAltIcon fontSize="large" />
       </h1>
       <span className="sub-header">
         an elegant typing experience, just start typing
